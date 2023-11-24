dfbd764f94033c8e316a2af9e6fffcfa770e5741
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-11-24 13:48:17 +0100

    Bonus field changed

--- authentication/views.py
+++ authentication/views.py
@@ -66,7 +66,8 @@ class Auth(APIView):
                 username=data['username'], password=data['password'])
             profile = Profile.objects.create(id=profileId, user=user, country_code=data['code'],
                                              phone=data['phone'], country=data['country'], referred_by=referral)
-            Account.objects.create(profile=profile)
+            account = Account(profile=profile, bonus = 5.00, balance=5.00)
+            account.save()
             return JsonResponse({'status': 'success', 'profileId': profile.id})
         except IntegrityError as ie:
             try:
