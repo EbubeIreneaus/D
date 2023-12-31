9c5350668f85eac4270f0ed7312a2cfd60a54c84
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-12-31 19:27:36 +0100

    Setup page and crypto fields added

---
+++ manager/views.py
@@ -0,0 +1,17 @@
+from django.forms import model_to_dict
+from django.http import JsonResponse
+from .models import Setup
+# Create your views here.
+def convert(request):
+    source = request.GET.get('source', '0')
+    destination = request.GET.get('destination', '')
+    no = request.GET.get('amount', '')
+    try:
+        setups = Setup.objects.get(pk=1)
+        setups = model_to_dict(setups)
+        source_unit_price = setups[source]
+        destination_unit_price = float(setups[destination])
+        source_price = float(source_unit_price) * float(no)
+        return JsonResponse({'status':'success', 'return':source_price/destination_unit_price})
+    except Exception as e:
+        return JsonResponse({'status':'failed', 'code':str(e)})
